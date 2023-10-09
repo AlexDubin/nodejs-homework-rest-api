474b8a1ac928d6049d09c88d1c47bf63d6dfa319
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-10-09 15:42:46 +0200

    avatar

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -2,10 +2,12 @@ const authSignUp = require("./authSignUp");
 const authSignIn = require("./authSignIn");
 const authCurrent = require("./authCurrent");
 const authSignOut = require("./authSignOut");
+const updateUserAvatar = require("./updateUserAvatar");
 
 module.exports = {
   authSignUp,
   authSignIn,
   authCurrent,
   authSignOut,
+  updateUserAvatar,
 };
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,10 +2,17 @@ const express = require("express");
 const auhRouter = express.Router();
 const authController = require("../../controllers/user/index");
 const authenticate = require("../../middlewares/authentic");
+const upload = require("../../middlewares/upload");
 
 auhRouter.post("/signup", authController.authSignUp.signUp);
 auhRouter.post("/signin", authController.authSignIn.signIn);
 auhRouter.get("/current", authenticate, authController.authCurrent.getCurrent);
 auhRouter.post("/signout", authenticate, authController.authSignOut.signOut);
+auhRouter.patch(
+  "/users/avatars",
+  authenticate,
+  upload.single("avatar"),
+  authController.updateUserAvatar.updateAvatar
+);
 
 module.exports = auhRouter;
